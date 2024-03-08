99f7da5185a0e9c78d9589544e9b86a0dcbebc87
firasabidli <abidlifiras@bizerte.r-iset.tn>
2024-03-08 17:22:56 +0100

    first commit

---
+++ backend/backend.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+//mongodb connection
+mongoose.connect('mongodb://127.0.0.1:27017/testImage',)
+  .then(() => console.log('Connected successfully to MongoDB !'))
+  .catch(() => console.log('Connection failed to MongoDB !'));
+
+//importing schema
+require("./imageDetails");
+const Images = mongoose.model("ImageDetails");
+
+app.get("/", async (req, res) => {
+  res.send("Success!!!!!!");
+});
+
+app.listen(5000, () => {
+  console.log("Server Started");
+});
+
+//////////////////////////////////////////////////////////////
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../src/images/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload-image", upload.single("image"), async (req, res) => {
+  console.log(req.body);
+  const imageName = req.file.filename;
+
+  try {
+    await Images.create({ image: imageName });
+    res.json({ status: "ok" });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
+
+app.get("/get-image", async (req, res) => {
+  try {
+    Images.find({}).then((data) => {
+      res.send({ status: "ok", data: data });
+    });
+  } catch (error) {
+    res.json({ status: error });
+  }
+});
